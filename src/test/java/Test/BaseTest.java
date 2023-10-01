5cc18eb7f7ed316dea574608a4ad9cf6b3fa3ee8
naveenraj39 <123942201+naveenraj39@users.noreply.github.com>
2023-10-01 19:09:08 +0530

    new file

---
+++ src/test/java/Test/BaseTest.java
@@ -0,0 +1,40 @@
+package Test;
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+import com.triomics.framework.TmxHubrisWreckerConfigLoader;
+import com.triomics.framework.common.AppConfigurations;
+
+import lombok.extern.log4j.Log4j2;
+
+import static com.codeborne.selenide.Selenide.open;
+
+
+import java.io.*;
+import java.util.Properties;
+
+@TestInstance(Lifecycle.PER_CLASS)
+// @Log4j2
+public class BaseTest{
+    @BeforeAll
+    @Test
+    public void beforeAll() throws IOException {
+        Properties config = new Properties();
+    	FileInputStream file = new FileInputStream (".//config/config.properties");
+    	config.load(file);
+    	String driver = config.getProperty("chromeDriver");
+    	System.setProperty("webdriver.chrome.driver", driver);
+    	
+   // 	configLoader.loadSelenideConfig();
+    //	String baseURL =(String) TmxHubrisWreckerConfigLoader.configurations.get(AppConfigurations.SELENIDE_BASEURL);
+    //	log.debug("Got baseURI as {}",baseURL);
+    	
+    	open("https://harmony.qa.triomics.in/");
+    }
+    
+    TmxHubrisWreckerConfigLoader configLoader=new TmxHubrisWreckerConfigLoader();
+}
---
+++ src/test/java/Test/LoginViaPlatformTest.java
@@ -0,0 +1,45 @@
+package Test;
+
+
+
+
+import Test.BaseTest;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.title;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+@TestInstance(Lifecycle.PER_CLASS)
+public class LoginViaPlatformTest extends BaseTest{
+
+
+   
+    @Test
+ 
+    public void login() {
+        title().equalsIgnoreCase("Harmony");
+        $(".MuiTypography-root.MuiTypography-subtitle1").shouldHave(text("Select Organization")).exists();
+       
+    }
+  @Test
+  
+    
+    public void login_as_platform_admin() throws InterruptedException {
+        $("div[role='button']").click();
+        $(byXpath("//span[normalize-space()='Test_Org_1']")).click();
+        $(".MuiButton-label").click();
+        
+    }
+
+
+ /*   protected void waitUntilPagesIsLoaded() {
+        $(byText("Sign in with your username and password")).should(appear, Duration.ofSeconds(2));
+    }
+*/
+}
