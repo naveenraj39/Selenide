fa5932fea99f299724c81dd3e219e276e186b852
naveenraj39 <123942201+naveenraj39@users.noreply.github.com>
2023-12-13 14:45:21 +0530

    xpath

--- src/test/java/Test/LoginViaPlatformTest.java
+++ src/test/java/Test/LoginViaPlatformTest.java
@@ -49,8 +49,9 @@ public class LoginViaPlatformTest extends BaseTest{
 
   @Test
   
-  public void enterCreds() {
-	  $(byXpath("(//*[@id='signInFormUsername'])[1]")).sendKeys("siteadm.qa@mailinator.com");
+  public void enterCreds() throws Exception {
+	  Thread.sleep(6000);
+	  $(byXpath("//*[@class='modal-content background-customizable modal-content-mobile visible-xs visible-sm']//child::div//child::input[@id='signInFormUsername']")).sendKeys("siteadm.qa@mailinator.com");
 	  $(byXpath("(//*[@id='signInFormPassword'])[1]")).sendKeys("Hello@123");
 	  $(byXpath("(//*[@name='signInSubmitButton'])[1]")).click();
   }
